8d32e24c3f56c6dee5dd769125c1100db4712c8b
alisa <alisakondakova0@gmail.com>
2025-01-26 10:59:47 +0800

    3

--- Кондакова.py
+++ Кондакова.py
@@ -13,8 +13,8 @@ def for_moving_scr(name):
     except pygame.error as message:
         print('Не удаётся загрузить:', name)
         raise SystemExit(message)
-    wid, hei = get_scr_size('first.png')
-    location = pygame.transform.scale(location, (wid, h))
+    wid, hei = get_scr_size(name)
+    location = pygame.transform.scale(location, (wid // 2, h))
     location = location.convert()
     return location
 
@@ -58,36 +58,32 @@ class Sprite(pygame.sprite.Sprite):
         pass
 
 class Player(Sprite):
-    def __init__(self, pos_x, pos_y):
+    def __init__(self, name, pos_x, pos_y):
         super().__init__(hero_group)
-        self.image = player_image
+        self.image = name
         self.rect = self.image.get_rect().move(pos_x, pos_y)
 
 
 sprite_group = pygame.sprite.Group()
 hero_group = pygame.sprite.Group()
-w, h = 800, 500
+w, h = 800, 600
 screen = pygame.display.set_mode((w, h))
 running = True
 locX = 0
 locY = 0
-widh = 100
-heih = 160
-widl, heil = get_scr_size('first.png')
-flipX = 0
-flip1X = -(widl-w)
-speed = 5
-fps = 80
+widh = 180
+heih = 360
+widl, heil = get_scr_size('forest.png')
+widl = widl // 2
+speed = 6
 moving = False
 to_left = False
 to_right = False
+loc = for_moving_scr('forest.png')
 while True:
-    clock = pygame.time.Clock()
-    pygame.time.delay(fps)
     screen.fill((255, 255, 255))
-    player_image = load_character_image('owls.png', widh, heih, -1)
-    owl = Player(w // 4, h // 4)
-    loc = for_moving_scr('first.png')
+    player_image = load_character_image('player.png', widh, heih, -1)
+    owl = Player(player_image, w // 6, h // 3)
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
@@ -104,19 +100,14 @@ while True:
             exit()
     if to_right is True:
         locX -= speed
-        flipX += speed
-        if locX < -(widl - w):
-            flip1X -= speed
     if to_left is True:
         locX += speed
-        flipX -= speed
-        flip1X += speed
     if locX > 0:
-        screen.blit(loc, (locX - widl, locY))
+        for i in range(1, 10):
+            screen.blit(loc, (locX - (widl * i), locY))
     if locX < 0:
-        screen.blit(loc, (locX + widl, locY))
-    print(locX)
-    clock.tick(fps)
+        for i in range(1, 10):
+            screen.blit(loc, (locX + (widl * i), locY))
     screen.blit(loc, (locX, locY))
     sprite_group.draw(screen)
     hero_group.draw(screen)
