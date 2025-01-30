de7e4f2c718b0468223026cee0a67d6aabe2a81e
alisa <alisakondakova0@gmail.com>
2025-01-30 20:37:22 +0800

    5

--- Кондакова.py
+++ Кондакова.py
@@ -51,13 +51,44 @@ class Sprite(pygame.sprite.Sprite):
 
 class Player(Sprite):
     def __init__(self, name, pos_x, pos_y):
-        super().__init__(hero_group)
+        super().__init__(first_g)
         self.image = name
         self.rect = self.image.get_rect().move(pos_x, pos_y)
 
+    def move(self, x, y):
+        self.rect = self.image.get_rect().move(x, y)
+
+
+class PlayerAn(Sprite):
+    def __init__(self, size, center):
+        super().__init__(first_g)
+        self.anim = list()
+        for i in range(1, 9):
+            self.anim.append(load_image(('0' + str(i) + '.png'), widh, heih))
+        self.image = self.anim[0]
+        self.rect = self.image.get_rect()
+        self.rect.center = center
+        self.fr = 0
+        self.last_update = pygame.time.get_ticks()
+        self.frame_rate = 200
+
+    def update(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_update > self.frame_rate:
+            self.last_update = now
+            self.fr += 1
+            if self.fr == len(self.anim):
+                self.fr = 0
+            self.image = self.anim[self.fr]
+
+    def rev(self):
+        self.image = pygame.transform.rotate(self.image, 180)
+        self.image = pygame.transform.flip(self.image, False, True)
+
+
 
 sprite_group = pygame.sprite.Group()
-hero_group = pygame.sprite.Group()
+first_g = pygame.sprite.Group()
 w, h = 800, 600
 screen = pygame.display.set_mode((w, h))
 running = True
@@ -80,8 +111,8 @@ clock = pygame.time.Clock()
 ch = pygame.Surface((800, 600))
 loc = load_image('forest.png', widl, h)
 loc2 = load_image('forest2.png', w, h, 0)
-player_image = load_image('player.png', widh, heih, -1)
-player = Player(player_image, w // 6, h // 3)
+player_image = load_image('01.png', widh, heih, -1)
+player = PlayerAn((180, 360), (w // 5, h // 1.5))
 player2_im = load_image('up.png', 100, 100, -1)
 player2 = Player(player2_im, w // 2.5, h // 1.5)
 while True:
@@ -107,15 +138,28 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    if to_right is True and to_up is False:
-        locX -= speed
-    if to_left is True and to_up is False:
-        locX += speed
+    if to_down is True:
+        if to_right or to_left:
+            player.update()
+        p2x = w // 2
+        p2y = h // 1.5
+        player2.move(p2x, p2y)
+        if to_right is True:
+            mov = 1
+            locX -= speed
+        if to_left is True:
+            locX += speed
     if to_up is True:
         if to_right is True:
             loc2Y += speed
+            if p2x <= 500:
+                p2x += speed
+                player2.move(p2x, p2y)
         if to_left is True:
             loc2Y += speed
+            if p2x >= 200:
+                p2x -= speed
+                player2.move(p2x, p2y)
     if locX > 0 and to_up is False:
         for i in range(1, 20):
             move(loc, locX - (widl * i), locY)
@@ -126,10 +170,14 @@ while True:
         for i in range(1, 20):
             move(loc2, loc2X, loc2Y - (h * i))
     if not to_up:
+        player2.remove(first_g)
+        player.add(first_g)
         move(loc, locX, locY)
     if not to_down:
+        player.remove(first_g)
+        player2.add(first_g)
         move(loc2, loc2X, loc2Y)
     screen.blit(ch, (0, 0))
     sprite_group.draw(screen)
-    hero_group.draw(screen)
+    first_g.draw(screen)
     pygame.display.flip()
